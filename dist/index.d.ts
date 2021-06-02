0a1ece28ed87b708385a3672ad9fdff39e011f23
Igor Khomenko <webigorkiev@gmail.com>
2021-06-02 13:08:03 +0300

    v1.0.6

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,6 @@
 import Validation, { ValidationFilters, ValidationModel, ValidatorsOptions, required, boolean, array, object, minlength, maxlength, rangelength, range, min, max, email, url, dateIso, digits, number, equal, eql, regexp, uaPhone, depends, rangedate } from "./Validation";
 import { ValidationError } from "./ValidationError";
 import { ValidationErrorMessage } from "./ValidationError";
-declare const validation: Validation;
+declare const validation: () => Validation;
 export { Validation, ValidationFilters, ValidationModel, ValidatorsOptions, ValidationError, ValidationErrorMessage, validation, required, boolean, array, object, minlength, maxlength, rangelength, range, min, max, email, url, dateIso, digits, number, equal, eql, regexp, uaPhone, depends, rangedate };
 export default validation;
